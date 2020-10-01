575d89fe049671f4fbc2c508b5164904667475e3
Alyssa <59900390+alyssahyq@users.noreply.github.com>
2020-10-01 16:44:46 -0400

    Add files via upload

---
+++ receiver.py
@@ -0,0 +1,91 @@
+import socket
+import sys
+import re
+import signal
+
+timeout = 0
+n_messages = 0
+n_bytes = 0
+sum = -1  # Get the sum of messages from sender
+
+def set_timeout(callback):
+    def wrap(func):
+        def handle(signum, frame):
+            raise RuntimeError
+
+        def to_do(*args, **kwargs):
+            try:
+                global timeout
+                signal.signal(signal.SIGALRM, handle)
+                signal.setitimer(signal.ITIMER_REAL, timeout)
+                r = func(*args, **kwargs)
+                signal.alarm(0)
+                return r
+            except RuntimeError as e:
+                callback()
+        return to_do
+    return wrap
+
+
+def after_timeout():
+    global n_messages
+    global n_bytes
+    print("Error: time out.")
+    print("Received ",n_messages," messages, ",n_bytes," bytes.")
+
+
+
+@set_timeout(after_timeout)
+def recv_loop(udp_socket,f):
+    global sum
+    global n_messages
+    global n_bytes
+    while True:
+        recv(udp_socket,f)
+        if sum == n_messages:
+            break
+    udp_socket.close()
+    print("Received ", n_messages, " messages, ", n_bytes, " bytes.")
+    return 0
+
+def recv(udp_socket,f):
+    global sum
+    recv_data = udp_socket.recvfrom(1024)
+    message = bytes.decode(recv_data[0])
+    if re.match("WILL SEND [0-9]+ MESSAGES",message):
+        sum = int(re.search("[0-9]+",message).group())
+    else:
+        f.write(message)
+        global n_messages
+        n_messages = n_messages+1
+        global n_bytes
+        n_bytes = n_bytes + len(message)
+
+def main():
+    command = sys.stdin.readline()
+    infos = (command.rstrip()).split(" ")
+    # receiver <file name> <timeout>
+    file_name = infos[1]
+    global timeout
+    timeout = float(infos[2])/1000.0
+
+    try:
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except:
+        print('Failed to get socket.', file=sys.stderr)
+    port = 8000
+    localaddr = ("", port)
+    print("Port: ",port)
+    with open('port', 'a') as p:
+        p.write(str(port))
+    try:
+        udp_socket.bind(localaddr)
+    except:
+        print('Failed to bind.', file=sys.stderr)
+    with open(file_name, 'a') as f:
+        recv(udp_socket,f)
+        recv_loop(udp_socket,f)
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ sender.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import os
+
+def main():
+    command = sys.stdin.readline()
+    infos = (command.rstrip()) .split(" ")
+    #sender <host> <port> <payload size> <file name>
+    host = infos[1]
+    port = infos[2]
+    payload_size = int(infos[3])
+    file_name = infos[4]
+    recv_addr = (host,int(port))
+
+    try:
+        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    except:
+        print('Failed to get socket.',file=sys.stderr)
+    file_size = os.path.getsize(file_name) #bytes
+    bytes_sent = 0
+    messages_sent = 0
+    with open(file_name, 'r') as f:
+        datagram_num = int(file_size/payload_size)+1
+        claim = "WILL SEND "+str(datagram_num)+" MESSAGES"
+        udp.sendto(bytes(claim, encoding='utf-8'),recv_addr)
+        for i in range(datagram_num):
+            buff = f.read(payload_size)
+            bytes_sent = bytes_sent+len(buff)
+            messages_sent = messages_sent+1
+            udp.sendto(bytes(buff, encoding='utf-8'), recv_addr)
+    print("Sent ",messages_sent," messages, ",bytes_sent, "bytes.")
+    udp.close()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
